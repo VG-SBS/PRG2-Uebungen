fb2aff206320bd8bd3831363fdb8e7fa82f6859e
VG-SBS <vincent.gundelwein.sbs@gmail.com>
2021-11-16 11:13:59 +0100

    MP3

--- 01_media.py
+++ 01_media.py
@@ -3,7 +3,7 @@ import os
 import pygame
 
 # Die Liste aller Dateien des Pfades aufrufen
-path = "/Users/student/music/"
+path = "/Users/student/music"
 
 #Variable mit dem Befehl der Ausgabe definieren
 dir_list = os.listdir(path)
@@ -23,18 +23,31 @@ print(songs)
 
 for i in range(len(dir_list)):
     print("{} - {}".format(i,songlist[i]))
-
+"""
 def songselection():
     selection = int(input("Bitte wählen Sie einen Song aus"))
 
     if selection <= songs-1:
         print(dir_list[selection])
-
-        os.system("afplay " + path + "/{}".format(dir_list[selection]))
+        Filetoplay = path + "/" + (dir_list[selection])
+        print(Filetoplay)
+        #os.system("afplay " + path + "/{}".format(dir_list[selection]))
     else:
         print("Titel konnte nicht gefunden werden")
         songselection()
 
 selection = songselection()
+"""
+
+selection = int(input("Bitte geben Sie eine Zahl ein"))
+Filetoplay = path + "/" + (dir_list[selection])
+print(Filetoplay)
+def pmusic():
+    file = "{}".format(Filetoplay)
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
 
 
+pmusic()
