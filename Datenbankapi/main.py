5c31fef3e21831f9f07041ce1f6885563e2d24f7
VG-SBS <vincent.gundelwein.sbs@gmail.com>
2022-02-15 08:17:47 +0100

    .

--- Datenbankapi/main.py
+++ Datenbankapi/main.py
@@ -1,7 +1,9 @@
 from datenbankapi import DB
 
+#db wird aus Klasse DB erstellt
 db = DB()
 
+#Player Hans wird erstellt und gibt Händler zurück(pk_max)
 pk_max = db.createPlayer("Hans")
 print(pk_max)
 
@@ -11,12 +13,13 @@ print(pk_max)
 pk_max = db.createPlayer("Wurst2")
 print(pk_max)
 
-
+#Playername wird anhand der PK ausgegeben
 player = db.getPlayer(1)
 print(player)
 
-
+#Player Score wird gesetzt (Scr, PK)
 player35 = db.setPlayerScore(100,5)
 
+#Playerscore wird anhand von pk ausgelesen und zurückgegeben
 player35 = db.getPlayerScore(5)
 print(player35)
