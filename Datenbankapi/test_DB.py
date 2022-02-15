dd57ccfcc0dbf44fab4afc6f6b5dcabff71aeaa0
VG-SBS <vincent.gundelwein.sbs@gmail.com>
2022-02-15 09:11:17 +0100

    Erster Unittest für DB_API

--- Datenbankapi/test_DB.py
+++ Datenbankapi/test_DB.py
@@ -1,19 +1,32 @@
 import unittest
 from datenbankapi import DB
 from main import *
+global first
 
 class TestDB(unittest.TestCase):
     def setUp(self):
         self.db = DB()
+        self.ret = ""
+        self.ret2 = 0
 
     def test_createPlayer(self):
-        first = self.db.createPlayer("Hans")
 
+        self.ret = self.db.createPlayer("Hans")
         second = -1
-
         message = "pk user Hans wrong"
 
-        self.assertGreater(first, second, message)
+        self.assertGreater(self.ret, second, message)
+
+    def test_getPlayer(self):
+        self.db2 = DB()
+        self.ret = self.db.createPlayer("Hans")
+        test = self.db2.getPlayer(self.ret)
+        second = "Hans"
+        message = "Name Falsch"
+        self.assertEqual(test,second,message)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
